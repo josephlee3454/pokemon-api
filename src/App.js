cfcf9bef76e80940f5ba950c385fbb55578149e0
Joe <joseph.lee3454@gmail.com>
2021-01-13 13:39:09 -0800

    added some stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import './App.css';
+import Pokemon from './componenets/Pokemon';
+
+
 
 function App() {
+
+  const [pokemon, setPokemon]= useState([{
+    name: "bulbasaur",
+    url: "https://pokeapi.co/api/v2/pokemon/1/"
+  }])
+
+  useEffect(() => {
+    // getPokemon();
+    getPokemonAxios();
+  }, [])
+  const getPokemon = () =>{
+    fetch("https://pokeapi.co/api/v2/pokemon/1")
+    .then(response => {
+      return response.json();
+  }).then(response => {
+      console.log(response.results);
+  }).catch(err=>{
+      console.log(err);
+  });
+  }
+  const getPokemonAxios = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon")
+      .then(res => {
+        console.log(res.data.results)
+        setPokemon(res.data.results);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        pokemon.map((pkm,idx)=>
+          <Pokemon pkm={pkm} key={idx} />
+        )
+      }
+      <button></button>
     </div>
   );
 }
---
+++ src/componenets/Pokemon.jsx
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const Pokemon = ({pkm}) => {
+
+    const [pokemon, setPokemon] = useState({
+        name:"",
+        sprites:{
+            front_default: ""
+        }
+
+    })
+
+
+    useEffect(()=>{
+        axios.get(pkm.url)
+            .then(res =>{
+                console.log(res)
+                setPokemon(res.data)
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+    },[]);
+    return (
+        <>
+            <div>
+                <p>
+                    {pokemon.name}
+                </p>
+                <img src = {pokemon.sprites.front_default} />
+            </div>
+        </>
+    )
+}
+
+export default Pokemon
